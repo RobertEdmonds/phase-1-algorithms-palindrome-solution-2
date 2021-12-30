c6e2e0209e068284cf165cc6767a1cd46f886c36
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-29 21:22:45 -0700

    commit tree 66879c3234c99b224aa728a3707e45dd4a3eb2a1

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning to the middle
+  for(let i=0;i<word.length/2;i++){
+    const j = word.length - 1 - i
+    const startWord = word[i]
+    const endWord = word[j]
+    if(startWord !== endWord)
+      return false
+    
+  }
+  return true
+  // check each letter to match the letter at the end
+  // if they don't match return false
+  // if they do return true
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning to the middle
+  check each letter to match the letter at the end
+  if they don't match return false
+  if they do return true
 */
 
 /*
   Add written explanation of your solution here
+  First letter matches the last letter and the second letter equal the second to last letter until the middle
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +38,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+
+  console.log("Expecting: True");
+  console.log("=>", isPalindrome("romor"));
 }
 
 module.exports = isPalindrome;
